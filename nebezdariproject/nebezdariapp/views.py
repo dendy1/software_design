9f30a0507c6eaf5be73ebb07bcaa182e0b207cae
Nikita <builovn@yandex.ru>
2020-10-12 17:43:05 +0300

    Alternative pagiantor
    Changed to constant count of pages in pagination bar

--- nebezdariproject/nebezdariapp/views.py
+++ nebezdariproject/nebezdariapp/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import HttpResponse, render
 from .models import Categories, Posts
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import EmptyPage, PageNotAnInteger
+from .lib.custom_paginator import CustomPaginator
 
 
 # Create your views here.
 def index(request):
-    index_categories_count = 10
-    posts_per_page = 5
+    index_categories_count = 10 #count of categories in categories bar
+    posts_per_page = 5 #count of posts on page
+    pagination_pages_range = 2 #count of pages right and left to the current page
     categories_list = Categories.objects.all()[:index_categories_count]
     posts_list = Posts.objects.all()
-    page_num = request.GET.get('page')
-    paginator = Paginator(posts_list, per_page=posts_per_page)
+    page_num = int(request.GET.get('page'))
+    paginator = CustomPaginator(posts_list, posts_per_page, pagination_pages_range)
     try:
         posts = paginator.page(page_num)
     except PageNotAnInteger:
@@ -18,10 +20,13 @@ def index(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
 
+    pagination_list = paginator.pagination_list()
+
     return render(request,
                   'main-page.html',
                   context={'categories_list': categories_list,
-                           'posts': posts,})
+                           'posts': posts,
+                           'pagination_list': pagination_list, })
 
 
 def post(request):
