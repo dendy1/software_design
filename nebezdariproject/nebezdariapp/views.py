30bb446f75a4cd55347c1190a9868c58aee0db18
Nikita <builovn@yandex.ru>
2020-10-12 05:56:33 +0300

    Pagination added

--- nebezdariproject/nebezdariapp/views.py
+++ nebezdariproject/nebezdariapp/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import HttpResponse, render
-from django.template import loader
 from .models import Categories, Posts
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def index(request):
     index_categories_count = 10
+    posts_per_page = 5
     categories_list = Categories.objects.all()[:index_categories_count]
     posts_list = Posts.objects.all()
+    page_num = request.GET.get('page')
+    paginator = Paginator(posts_list, per_page=posts_per_page)
+    try:
+        posts = paginator.page(page_num)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,
                   'main-page.html',
                   context={'categories_list': categories_list,
-                           'posts_list': posts_list})
+                           'posts': posts,})
+
 
 def post(request):
     return render(request,
