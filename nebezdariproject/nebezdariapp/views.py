29a0de50e0de235fa8a8348ec786d70b71960e96
Nikita <builovn@yandex.ru>
2020-10-20 04:00:32 +0300

    Добавлена отправка уведомлений на почтовые ящики подписавшихся

--- nebezdariproject/nebezdariapp/views.py
+++ nebezdariproject/nebezdariapp/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from .lib.custom_paginator import CustomPaginator
+from .lib.mail.mass_mailing import subscribers_mass_mail
 from .models import Category, Post, Author, Comment, MailingMember
 from django.core.mail import send_mass_mail, send_mail, BadHeaderError
 from .forms import PostForm, LoginForm, NewAuthorForm, ContactForm, EditAuthorForm, SubscribeForm
@@ -118,6 +119,10 @@ def post_add(request):
             post.author = request.user
             post.save()
             form.save_m2m()
+
+            subscribers_message = "Вышел новый пост: "
+            link = "https://www.nebezdari.ru/post/" + str(post.id)
+            subscribers_mass_mail(subscribers_message, link=link)
 
             return HttpResponseRedirect('/author/')
     else:
